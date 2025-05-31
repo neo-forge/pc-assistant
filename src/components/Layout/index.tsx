5ec7170d21ba33c1e19492527a328147e911a91f
shanlihang <67490009@qq.com>
2025-05-31 17:11:55 +0800

    feat：封装页面布局组件

---
+++ src/components/Layout/index.tsx
@@ -0,0 +1,37 @@
+import { Layout } from "antd";
+import { Outlet } from "react-router-dom";
+import Sidebar from "./Sidebar";
+const { Sider, Content } = Layout;
+
+const PageLayout = () => {
+  return (
+    <Layout
+      style={{
+        width: "100%",
+        height: "100vh",
+        overflow: "hidden",
+        display: "flex",
+      }}
+    >
+      <Layout>
+        <Sider
+          width="80"
+          style={{
+            backgroundColor: "transparent",
+          }}
+        >
+          <Sidebar />
+        </Sider>
+        <Content
+          style={{
+            flex: 1,
+          }}
+        >
+          <Outlet />
+        </Content>
+      </Layout>
+    </Layout>
+  );
+};
+
+export default PageLayout;
